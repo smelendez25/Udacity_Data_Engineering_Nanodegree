0d36b488d0331d19ee22c9d85faf06b6e17ab974
Steven Melendez <smelendez25@users.noreply.github.com>
2021-01-20 20:44:57 -0600

    Update etl.py

--- Project_01/etl.py
+++ Project_01/etl.py
@@ -6,19 +6,49 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    # open song file
-    df = pd.read_json(filepath , lines = True)
-
-    # insert song record
-    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
-    cur.execute(song_table_insert, song_data)
+    """Processes a song file.
     
-    # insert artist record
-    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
-    cur.execute(artist_table_insert, artist_data)
+    Artist information is inserted into the 'artists' table. And Song information
+    is inserted into the 'songs' table.
+    
+    Args:
+        cur (psycopg2.cursor): A database cursor
+        filepath (str): A filepath to a song file
+    """
+    
+    # open song file
+    df = pd.read_json(filepath, lines=True)
+    
+    for index, row in df.iterrows():
+        # insert song record
+        song_data = (row.song_id, row.title, row.artist_id, row.year, row.duration)
+        try:
+            cur.execute(song_table_insert, song_data)
+        except psycopg2.Error as e:
+            print("Error: Inserting row for table: songs")
+            print (e)
+    
+        # insert artist record
+        artist_data = (row.artist_id, row.artist_name, row.artist_location, row.artist_latitude, row.artist_longitude)
+        try:
+            cur.execute(artist_table_insert, artist_data)
+        except psycopg2.Error as e:
+            print("Error: Inserting row for table: artists")
+            print (e)
 
 
 def process_log_file(cur, filepath):
+    """Processes a log file.
+    
+    Time information is inserted into the 'time' table. User information
+    is upserted into the 'users' table. And Songplay information is
+    inserted into the 'songplays' table.
+    
+    Args:
+        cur (psycopg2.cursor): A database cursor
+        filepath (str): A filepath to a log file
+    """
+    
     # open log file
     df = pd.read_json(filepath, lines=True)
 
@@ -26,36 +56,74 @@ def process_log_file(cur, filepath):
     df = df[df.page == 'NextSong']
 
     # convert timestamp column to datetime
-    t = pd.to_datetime(df['ts'] , unit='ms')
+    df['ts'] = pd.to_datetime(df['ts'], unit='ms')
+    t = df.copy()
     
     # insert time data records
-    time_data = (t, t.dt.hour, t.dt.day, t.dt.week , t.dt.month, t.dt.year, t.dt.weekday)
-    column_labels = ('timestamp', 'hour', 'day', 'week of year', 'month', 'year', 'weekday')
-    time_df = pd.DataFrame.from_dict(dict(zip(column_labels,time_data)))
+    time_data = (t.ts, t.ts.dt.hour , t.ts.dt.day , t.ts.dt.dayofweek , t.ts.dt.month , t.ts.dt.year , t.ts.dt.weekday)
+    column_labels = ['start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday']
+    time_df = pd.DataFrame(columns=column_labels)
+
+    # Assign datetime into time dataframe
+    for index, column_label in enumerate(column_labels):
+        time_df[column_label] = time_data[index]
 
     for i, row in time_df.iterrows():
-        cur.execute(time_table_insert, list(row))
+        try:
+            cur.execute(time_table_insert, list(row))
+        except psycopg2.Error as e:
+            print("Error: Inserting row for table: time")
+            print (e)
 
     # load user table
     user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
 
     # insert user records
     for i, row in user_df.iterrows():
-        cur.execute(user_table_insert, row)
+        try:
+            cur.execute(user_table_insert, row)
+        except psycopg2.Error as e:
+            print("Error: Inserting row for table: users")
+            print (e)
 
     # insert songplay records
     for index, row in df.iterrows():
         
         # get songid and artistid from song and artist tables
-        results = cur.execute(song_select, (row.song, row.artist, row.length))
-        songid, artistid = results if results else None, None
+        try:
+            cur.execute(song_select, (row.song, row.artist, row.length))
+            results = cur.fetchone()
+        
+            if results:
+                songid, artistid = results
+            else:
+                songid, artistid = None, None
 
-        # insert songplay record
-        songplay_data = (index, row.ts, int(row.userId), row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
-        cur.execute(songplay_table_insert, songplay_data)
+            # insert songplay record
+            songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+            try:
+                cur.execute(songplay_table_insert, songplay_data)
+            except psycopg2.Error as e:
+                print("Error: Inserting row for table: songplays")
+                print (e)
+        except psycopg2.Error as e:
+            print("Error: Querying for Song ID and Artist ID")
+            print (e)
 
 
 def process_data(cur, conn, filepath, func):
+    """Processes JSON files for a data directory path.
+    
+    Valid function values can be 'process_song_file' or
+    'process_log_file'.
+    
+    Args:
+        cur (psycopg2.cursor): A database cursor
+        conn (psycopg2.connection): A database connection
+        filepath (str): A filepath of the directory to process
+        func (function): The function to call for each found file
+    """
+    
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -75,12 +143,28 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
-    cur = conn.cursor()
-
+    """Script main method.
+    
+    Creates a database connection, processes Song and Log information, and then
+    closes the cursor and database connection.
+    """
+    
+    try:
+        conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    except psycopg2.Error as e:
+        print("Error: Could not make connection to the Postgres database")
+        print(e)
+    
+    try:
+        cur = conn.cursor()
+    except psycopg2.Error as e:
+        print("Error: Could not get curser to the Database")
+        print(e)
+    
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
+    cur.close()
     conn.close()
 
 
