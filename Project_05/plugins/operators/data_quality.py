08bfab8a2380ab3d0170f370584b027bdf2e6922
Steven Melendez <smelendez25@users.noreply.github.com>
2021-05-11 11:56:46 -0600

    updated version

--- Project_05/plugins/operators/data_quality.py
+++ Project_05/plugins/operators/data_quality.py
@@ -2,49 +2,41 @@ from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-
 class DataQualityOperator(BaseOperator):
+    """
+    Runs data quality check by passing test SQL query and expected result
+    
+    :param redshift_conn_id: Redshift connection ID
+    :param test_query: SQL query to run on Redshift data warehouse
+    :param expected_result: Expected result to match against result of
+        test_query
+    """
+    
     ui_color = '#89DA59'
-
+    
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="",
-                 table="",
-                 test_stmt=None,
-                 result=None,
+                 test_query="",
+                 expected_result="",
                  *args, **kwargs):
-
+        
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
-        self.table = table
-        self.test_stmt = test_stmt
-        self.result = result
-
+        self.test_query = test_query
+        self.expected_result = expected_result
+    
     def execute(self, context):
-        """
-        Perform data quality checks on resulting fact and dimension tables.
-
-        Parameters:
-        ----------
-        redshift_conn_id: string
-            airflow connection to redshift cluster
-        table: string
-            table located in redshift cluster
-        test_stmt: string
-            test SQL command to check validity of target table
-        result: string
-            result of test_stmt to check validity
-        """
-        pg_hook = PostgresHook(self.redshift_conn_id)
-        records = pg_hook.get_records(f"SELECT COUNT(*) FROM {self.table}")
-        if len(records) < 1 or len(records[0]) < 1:
-            raise ValueError(f"Fail: No results for {self.table}")
-        num_records = records[0][0]
-        if num_records < 1:
-            raise ValueError(f"Fail: 0 rows in {self.table}")
-
-        if self.test_stmt:
-            output = pg_hook.get_first(self.test_stmt)
-            if self.result != output:
-                raise ValueError(f"Fail: {output} != {self.result}")
-        self.log.info(f"Success: {self.table} has {records[0][0]} records")
+        
+        self.log.info("Getting credentials")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        
+        self.log.info("Running test")
+        records = redshift_hook.get_records(self.test_query)
+        if records[0][0] != self.expected_result:
+            raise ValueError(f"""
+                Data quality check failed. \
+                {records[0][0]} does not equal {self.expected_result}
+            """)
+        else:
+            self.log.info("Data quality check passed")
--- Project_05/plugins/operators/stage_redshift.py
+++ Project_05/plugins/operators/stage_redshift.py
@@ -1,89 +1,64 @@
+  
 from airflow.hooks.postgres_hook import PostgresHook
-from airflow.hooks.S3_hook import S3Hook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 
-
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    
     template_fields = ("s3_key",)
     copy_sql = """
-        COPY {0}
-        FROM '{1}'
-        ACCESS_KEY_ID '{2}'
-        SECRET_ACCESS_KEY '{3}'
-        {4}
-        {5}
-        {6}
-        TIMEFORMAT as 'epochmillisecs'
-        TRUNCATECOLUMNS BLANKSASNULL EMPTYASNULL
+        COPY {table}
+        FROM '{s3_path}'
+        ACCESS_KEY_ID '{key_id}'
+        SECRET_ACCESS_KEY '{access_key}'
+        FORMAT AS {file_format} 
     """
 
     @apply_defaults
     def __init__(self,
-                 redshift_conn_id="redshift",
-                 aws_credentials_id="aws_credentials",
-                 table="staging_temp",
-                 s3_bucket="udacity-dend",
-                 s3_key="temp",
-                 file_format="JSON",
+                 redshift_conn_id="",
+                 aws_credentials_id="",
+                 table="",                 
+                 s3_bucket="",
+                 s3_key="",
+                 file_format="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        self.redshift_conn_id = redshift_conn_id
-        self.aws_credentials_id = aws_credentials_id
-        self.table = table
-        self.s3_bucket = s3_bucket
-        self.s3_key = s3_key
-        self.file_format = file_format
+        self.redshift_conn_id=redshift_conn_id
+        self.aws_credentials_id=aws_credentials_id
+        self.table=table
+        self.s3_bucket=s3_bucket
+        self.s3_key=s3_key
+        self.file_format=file_format
 
     def execute(self, context):
-        """
-        Copy data from S3 buckets to redshift cluster into staging tables.
-
-        Parameters:
-        ----------
-        redshift_conn_id: string
-            airflow connection to redshift cluster
-        aws_credentials_id: string
-            airflow connection to AWS
-        table: string
-            target table located in redshift cluster
-        s3_bucket: string
-            bucket location of staging data
-        s3_key: boolean
-            path location of staging data
-        file_format: string
-            file format to copy data, default JSON
-        """
-        self.log.info(f"Begin {self.table} StageToRedshiftOperator")
-        pg_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        s3_hook = S3Hook(aws_conn_id=self.aws_credentials_id)
-        credentials = s3_hook.get_credentials()
-
-        self.log.info(f"Copying {self.table} from S3 to Redshift")
-        s3_path = "s3://{}/{}/".format(self.s3_bucket, self.s3_key)
-        if self.file_format == 'CSV':
-            format = "CSV"
-            JSON = "s3://udacity-dend/log_json_path.json"
-            delimiter = "DELIMITER ','"
-            header = "IGNOREHEADER 1"
-        else:
-            format = "FORMAT AS JSON 'auto'"
-            JSON = ""
-            delimiter = ""
-            header = ""
-
+        try:
+            aws_hook = AwsHook(self.aws_credentials_id)
+            credentials = aws_hook.get_credentials()
+            self.log.info("AWS Hook Defined")
+            redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+            self.log.info("Redshift Hook Defined")
+        except AirflowError as e:
+            self.log.error(e)
+        
+        # Clearing data from staging table
+        self.log.info(f"Clearing data from {self.table}")
+        redshift.run(f"DELETE FROM {self.table}")
+        
+        # Rendering S3 according to the formated values given
+        rendered_key = self.s3_key.format(**context)
+        s3_path = "s3://{}/{}".format(self.s3_bucket, rendered_key)
+        
+        # Copying data from S3 to Redshift
+        self.log.info(f"Copying data from {s3_path} to Redshift")
         formatted_sql = StageToRedshiftOperator.copy_sql.format(
-            self.table,
-            s3_path,
-            credentials.access_key,
-            credentials.secret_key,
-            format,
-            JSON,
-            delimiter,
-            header
+            table=self.table,
+            s3_path=s3_path,
+            key_id=credentials.access_key,
+            access_key=credentials.secret_key,
+            file_format=self.file_format
         )
-        self.log.info(f"QUERY: {formatted_sql}")
-        pg_hook.run(formatted_sql)
-        self.log.info(f"Success: Copying {self.table} from S3 to Redshift")
+        redshift.run(formatted_sql)
